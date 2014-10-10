39703078fd9a45c337a7f60e4e64df0aaf173100
Damon E. Drake <damon.e.drake@gmail.com>
2014-10-10 13:27:30 -0500

    Initial Project Upload.

---
+++ TestConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Smelds.Barcodes;
+
+namespace ConsoleApp {
+  public class Program {
+    public static void Main(string[] args) {
+
+      using (var b = new Bookland("9780816155293")) {
+        b.SaveAs("9780816155293.png", true);
+      }
+
+      Console.WriteLine("043942089X".ISBNConvert10To13());
+      Console.WriteLine("978034553898".ISBNConvert13to10());
+
+      Console.ReadKey(true);
+    }
+  }
+
+}
