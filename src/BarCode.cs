d72ecd49704952d32961b958eaf3af391bac22e0
Damon Drake <damon.drake@evergy.com>
2021-12-10 04:33:46 -0600

    fixed editor tabbing frenzy

--- src/BarCode.cs
+++ src/BarCode.cs
@@ -4,79 +4,79 @@ using System.Drawing.Imaging;
 using System.Text;
 
 namespace DEDrake.Barcodes {
-	public class Barcode : IDisposable {
-		protected Image? barcodeImage = null;
-		protected StringBuilder? BinaryText = null;
-		protected int LineBuffer = 0;
-		protected MemoryStream? ms = null;
-		protected string? Code = null;
+  public class Barcode : IDisposable {
+    protected Image? barcodeImage = null;
+    protected StringBuilder? BinaryText = null;
+    protected int LineBuffer = 0;
+    protected MemoryStream? ms = null;
+    protected string? Code = null;
 
-		public virtual byte[] BinaryImage {
-			get {
-				if (ms.Length == 0) RenderBarcode(); return ms.ToArray();
-			}
-		}
+    public virtual byte[] BinaryImage {
+      get {
+        if (ms.Length == 0) RenderBarcode(); return ms.ToArray();
+      }
+    }
 
-		public Image BarcodeImage {
-			get {
-				if (ms.Length == 0) RenderBarcode(); if (barcodeImage == null) barcodeImage = Image.FromStream(ms); return barcodeImage;
-			}
-		}
+    public Image BarcodeImage {
+      get {
+        if (ms.Length == 0) RenderBarcode(); if (barcodeImage == null) barcodeImage = Image.FromStream(ms); return barcodeImage;
+      }
+    }
 
-		public Barcode(string code) {
-			ms = new MemoryStream();
-			BinaryText = new StringBuilder();
-			LineBuffer = 21;
-			Code = code;
-		}
+    public Barcode(string code) {
+      ms = new MemoryStream();
+      BinaryText = new StringBuilder();
+      LineBuffer = 21;
+      Code = code;
+    }
 
-		public void SaveAs(string FilePath, bool OverWrite = false) {
-			if (File.Exists(FilePath) && !OverWrite) throw new IOException("File exits.");
-			using var fs = new FileStream(FilePath, FileMode.Create, FileAccess.Write); fs.Write(BinaryImage, 0, BinaryImage.Length);
-		}
+    public void SaveAs(string FilePath, bool OverWrite = false) {
+      if (File.Exists(FilePath) && !OverWrite) throw new IOException("File exits.");
+      using var fs = new FileStream(FilePath, FileMode.Create, FileAccess.Write); fs.Write(BinaryImage, 0, BinaryImage.Length);
+    }
 
-		protected virtual void RenderBarcode() {
-			var binary = BinaryText.ToString().ToCharArray();
-			var width = binary.Length + 41;
+    protected virtual void RenderBarcode() {
+      var binary = BinaryText.ToString().ToCharArray();
+      var width = binary.Length + 41;
 
-			using var bmp = new Bitmap(width, 105, PixelFormat.Format32bppRgb);
-			using (var pen = new Pen(Color.White)) using (var g = Graphics.FromImage(bmp)) {
-				g.Clear(Color.White);
+      using var bmp = new Bitmap(width, 105, PixelFormat.Format32bppRgb);
+      using (var pen = new Pen(Color.White)) using (var g = Graphics.FromImage(bmp)) {
+        g.Clear(Color.White);
 
-				var r = new Rectangle(10, 72, 10, 15);
-				g.SmoothingMode = SmoothingMode.AntiAlias;
-				g.InterpolationMode = InterpolationMode.HighQualityBicubic;
-				g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+        var r = new Rectangle(10, 72, 10, 15);
+        g.SmoothingMode = SmoothingMode.AntiAlias;
+        g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+        g.PixelOffsetMode = PixelOffsetMode.HighQuality;
 
-				r = new Rectangle(0, 72, width, 15);
-				var sf = new StringFormat {
-					LineAlignment = StringAlignment.Center,
-					Alignment = StringAlignment.Center
-				};
+        r = new Rectangle(0, 72, width, 15);
+        var sf = new StringFormat {
+          LineAlignment = StringAlignment.Center,
+          Alignment = StringAlignment.Center
+        };
 
-				g.DrawString(Code.ToUpper(), new Font("Courier", 8), Brushes.Black, r, sf);
+        g.DrawString(Code.ToUpper(), new Font("Courier", 8), Brushes.Black, r, sf);
 
-				g.Flush();
+        g.Flush();
 
-				foreach (var c in binary) {
-					pen.Color = c == '1' ? Color.Black : Color.White;
-					g.DrawLine(pen, LineBuffer, 20, LineBuffer, 70);
-					LineBuffer++;
-				}
-			}
+        foreach (var c in binary) {
+          pen.Color = c == '1' ? Color.Black : Color.White;
+          g.DrawLine(pen, LineBuffer, 20, LineBuffer, 70);
+          LineBuffer++;
+        }
+      }
 
-			bmp.Save(ms, ImageFormat.Png);
-		}
+      bmp.Save(ms, ImageFormat.Png);
+    }
 
-		protected virtual void Dispose(bool disposing) {
-			if (disposing) {
-				if (BarcodeImage != null) BarcodeImage.Dispose(); if (ms != null) ms.Dispose();
-			}
-		}
+    protected virtual void Dispose(bool disposing) {
+      if (disposing) {
+        if (BarcodeImage != null) BarcodeImage.Dispose(); if (ms != null) ms.Dispose();
+      }
+    }
 
-		public void Dispose() {
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
-	}
+    public void Dispose() {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+  }
 }
--- src/Utils/ISBN.cs
+++ src/Utils/ISBN.cs
@@ -1,57 +1,57 @@
 ﻿using System.Text.RegularExpressions;
 
 namespace DEDrake.Barcodes.Utils {
-	public enum ISBNFormat {
-		None = 0,
-		Hyphens = 1,
-		Spaces = 2
-	}
+  public enum ISBNFormat {
+    None = 0,
+    Hyphens = 1,
+    Spaces = 2
+  }
 
-	public static class ISBN {
-		public static string ISBNConvert10To13(this string isbn10, ISBNFormat format = ISBNFormat.None) {
-			var isbn = isbn10.Replace(" ", "").Replace("-", "");
+  public static class ISBN {
+    public static string ISBNConvert10To13(this string isbn10, ISBNFormat format = ISBNFormat.None) {
+      var isbn = isbn10.Replace(" ", "").Replace("-", "");
 
-			if (!Regex.IsMatch(isbn, @"^(\d{9}|\d{10}|\d{9}[Xx])$"))
-				throw new FormatException("ISBN 10 must be either 9 or 10 digits optinally with an x or X as the 10 character.");
+      if (!Regex.IsMatch(isbn, @"^(\d{9}|\d{10}|\d{9}[Xx])$"))
+        throw new FormatException("ISBN 10 must be either 9 or 10 digits optinally with an x or X as the 10 character.");
 
-			var s = "978" + isbn[..9];
-			var k = 0;
+      var s = "978" + isbn[..9];
+      var k = 0;
 
-			for (var i = 1; i <= s.Length; i++) k += i % 2 == 0 ? int.Parse(s[i - 1].ToString()) * 3 : int.Parse(s[i - 1].ToString());
+      for (var i = 1; i <= s.Length; i++) k += i % 2 == 0 ? int.Parse(s[i - 1].ToString()) * 3 : int.Parse(s[i - 1].ToString());
 
-			var converted = s + (10 - k % 10).ToString();
+      var converted = s + (10 - k % 10).ToString();
 
-			return format switch {
-				ISBNFormat.Hyphens => converted,
-				ISBNFormat.Spaces => converted,
-				ISBNFormat.None => converted,
-				_ => converted,
-			};
-		}
+      return format switch {
+        ISBNFormat.Hyphens => converted,
+        ISBNFormat.Spaces => converted,
+        ISBNFormat.None => converted,
+        _ => converted,
+      };
+    }
 
-		public static string ISBNConvert13to10(this string isbn13, ISBNFormat format = ISBNFormat.None) {
-			var isbn = isbn13.Replace(" ", "").Replace("-", "");
+    public static string ISBNConvert13to10(this string isbn13, ISBNFormat format = ISBNFormat.None) {
+      var isbn = isbn13.Replace(" ", "").Replace("-", "");
 
-			if (!Regex.IsMatch(isbn, @"^(\d{12}|\d{13})$"))
-				throw new FormatException("ISBN 13 must be either 12 or 13 digits.");
+      if (!Regex.IsMatch(isbn, @"^(\d{12}|\d{13})$"))
+        throw new FormatException("ISBN 13 must be either 12 or 13 digits.");
 
-			var s = isbn.Substring(3, 9);
-			var k = 0;
-			var x = 0;
+      var s = isbn.Substring(3, 9);
+      var k = 0;
+      var x = 0;
 
-			for (var i = 10; i >= 2; i--) {
-				k += i * int.Parse(s[x].ToString());
-				x++;
-			}
+      for (var i = 10; i >= 2; i--) {
+        k += i * int.Parse(s[x].ToString());
+        x++;
+      }
 
-			var converted = s + (11 - k % 11 == 10 ? "X" : (11 - k % 11).ToString());
+      var converted = s + (11 - k % 11 == 10 ? "X" : (11 - k % 11).ToString());
 
-			return format switch {
-				ISBNFormat.Hyphens => converted,
-				ISBNFormat.Spaces => converted,
-				ISBNFormat.None => converted,
-				_ => converted,
-			};
-		}
-	}
+      return format switch {
+        ISBNFormat.Hyphens => converted,
+        ISBNFormat.Spaces => converted,
+        ISBNFormat.None => converted,
+        _ => converted,
+      };
+    }
+  }
 }
